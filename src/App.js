8a8013305414a54cac2a14a449667aaa971d96af
silijove <42864526+silijove@users.noreply.github.com>
2018-12-14 15:51:42 +0100

    Diff router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
 import React from 'preact-compat';
-import './App.css';
-import AppRouter from './AppRouter';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <AppRouter />;
-}
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+
+const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
+Topic.propTypes = {
+  match: PropTypes.func.isRequired,
+};
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+
+    <ul>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:id`} component={Topic} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+Topics.propTypes = {
+  match: PropTypes.func.isRequired,
+};
+
+const Header = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/about">About</Link>
+    </li>
+    <li>
+      <Link to="/topics">Topics</Link>
+    </li>
+  </ul>
+);
+
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
 
 export default App;
